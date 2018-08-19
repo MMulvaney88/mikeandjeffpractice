57ac2a6a5ab5a7b7f348702fdcf870a38328aae5
Michael Mulvaney <MMulvaney88@gmail.com>
2018-08-19 15:19:28 -0400

    Creating basic class for "Bank Accounts." Added DEPOSIT and WITHDRAWAL methods

---
+++ bankaccounts.py
@@ -0,0 +1,25 @@
+#vars () = retrieves all properties from a class
+
+
+class Account(object):
+    def __init__(self, name, balance = 0):
+        self.name = name
+        self.balance = balance
+
+    def deposit(self, balance):
+        self.balance = self.balance + balance
+
+    def withdrawal(self, balance):
+        self.balance = self.balance - balance
+
+John = Account("John", 500)
+print John.name
+print John.balance
+
+John.deposit(1000)
+
+print John.balance
+
+John.withdrawal(1500)
+
+print John.balance
