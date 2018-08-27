540f691823b426ee14395d804e49c834c7154f74
Michael Mulvaney <MMulvaney88@gmail.com>
2018-08-27 15:46:11 -0400

    Revised DEPOSIT and WITHDRAWAL. Created shell for TRANSFER function.

--- bankaccounts.py
+++ bankaccounts.py
@@ -8,38 +8,38 @@ class Account(object):
         self.balance = balance
 
     def deposit(self, amount):
+        #The DEPOSIT is added to the balance. Message is given on new amount
         self.balance = self.balance + amount
+        print "$%s has been added to your account." %(amount)
+        print "Your new balance is $%s." %(self.balance)
+        print "Have a nice day, %s" %(self.name)
 
     def withdrawal(self, amount):
+        #The WITHDRAWL is deducted from balance. Message is given on new amount
+        #If account is left <0, withdrawal is cancelled.
+        self.balance = self.balance - amount
         if (self.balance < 0):
             self.balance = self.balance + amount
             print "Your withdrawal will cause an overdraft. Please try again."
+            print "Your balance is currently at %s." %(self.balance)
         else:
-            self.balance = self.balance - amount
-            print "Please find your withdrawal for %s!" %(amount)
-            print "Your account is now at %s." %(self.balance)
+            print "Please find your withdrawal for $%s!" %(amount)
+            print "Your account is now at $%s." %(self.balance)
             print "Have a nice day, %s!" %(self.name)
 
-
-
-
-John = Account("John", 500)
-print John.name
-print John.balance
-
-John.withdrawal(100)
-
+    #Transfer function - To Be Completed
+    #def transfer(self, amount, newAccount):
+        #select primary Account
+        #WITHDRAW $$$ from primary Account
+        #DEPOSIT $$$ into newAccount
 
 '''
-John.deposit(1000)
-
-print John.balance
-
-John.withdrawal(50)
-
-print John.balance
-
-John.withdrawal(1500)
-
-print John.balance
+#Testing the DEPOSIT function
+Jeff = Account("Jeff Carroll", 2000)
+Jeff.deposit(1000)
+'''
+'''
+#Testing the WITHDRAWAL function
+John = Account("John", 500)
+John.withdrawal(100)
 '''
